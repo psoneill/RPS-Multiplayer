5eebd4a2fe7bc8f5ed642ff714d5c7230a031757
Patrick ONeill <poneill@sembracare.com>
2019-10-01 22:16:03 -0400

    adding base layout and javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+var config = {
+    apiKey: "AIzaSyDP8V8qW35Sc-fXMdKRPcC3SG0SBxShBQk",
+    authDomain: "rockpaperscissors-94349.firebaseapp.com",
+    databaseURL: "https://rockpaperscissors-94349.firebaseio.com",
+    projectId: "rockpaperscissors-94349",
+    storageBucket: "",
+    messagingSenderId: "948732802017",
+    appId: "1:948732802017:web:ab38ba34c528882d2518a0",
+    measurementId: "G-E6FFPT0G1M"
+};
+
+firebase.initializeApp(config);
+
+
+// Create a variable to reference the database.
+var database = firebase.database();
+
+$(document).ready(function () {
+
+
+
+});
